ee7b0697607d14ffd14ad6b14c7e435a4a34bed9
lorenaR02 <52755291+lorenaR02@users.noreply.github.com>
2019-07-31 11:25:32 -0700

    Add files via upload

---
+++ homebutton.js
@@ -0,0 +1,12 @@
+function pubertyMenstruation() {
+  document.getElementById("content1").style.display="inline-block";
+  document.getElementById("content1").style.align="center";
+  }
+  function communicate() {
+  document.getElementById("content2").style.display="inline-block";
+  document.getElementById("content2").style.align="center";
+    }
+  function sexualHealth() {
+  document.getElementById("content3").style.display="inline-block";
+  document.getElementById("content3").style.align="center";
+     }
